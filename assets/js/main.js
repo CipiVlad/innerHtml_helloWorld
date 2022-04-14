6ae609e7e4b0eaea3865f3fd9f3608c2b30c765f
CipiVlad <supercoder.cipi@gmail.com>
2022-04-14 13:57:09 +0200

    fuction_helloworld

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
-document.getElementById("info").innerHTML = "<h1>hello world</h1>";
-document.getElementById("question").innerHTML = "<h2>How are you?</h2>";
-document.getElementById("container").innerHTML = " <p>start of the element</p>";
+function intro() {
+    let a = 1 + 3;
+    console.log("Hello World!");
+    console.log("1+3 = " + a);
+};
 
-//Mit document.write() fügt man immer am Ende des HTML Dokuments die Inhalte hinzu.
-document.write("end of the element");
+intro();
+
