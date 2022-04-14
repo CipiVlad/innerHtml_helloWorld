621abb27ef7708433c5a31b65cf9aaf429f872a2
CipiVlad <supercoder.cipi@gmail.com>
2022-04-14 12:59:27 +0200

    start_end_doc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,6 @@
 document.getElementById("info").innerHTML = "<h1>hello world</h1>";
 document.getElementById("question").innerHTML = "<h2>How are you?</h2>";
+document.getElementById("container").innerHTML = " <p>start of the element</p>";
+
+//Mit document.write() fügt man immer am Ende des HTML Dokuments die Inhalte hinzu.
+document.write("end of the element");
